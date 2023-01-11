48766bbc9bb8618a3cfa40792449d62b1ff26140
Nathan <nathancoats@live.co.uk>
2023-01-11 14:38:28 +0000

    added nicer scrolling, made app a class

--- src/App.js
+++ src/App.js
@@ -15,28 +15,86 @@ import Projects from "./Projects";
 import ContactMe from "./ContactMe";
 import {Reveal} from "react-reveal";
 
+
+/*function App() {*/
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        /*this.NavBarStyle = this.NavBarStyle.bind(this);*/
+        this.handleScroll = this.handleScroll.bind(this);
+        this.state = {
+            'scrolling': false,
+            'currentY': window.scrollY
+        };
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+    handleScroll(event) {
+        let currentY = window.scrollY;
+        if (currentY > this.state.currentY) {
+            this.setState({
+                scrolling: true,
+                currentY: currentY
+            });
+        }
+        else if (window.scrollY <= this.state.currentY){
+            this.setState({
+                scrolling: false,
+                currentY: currentY
+            });
+        }
+    }
+    NavBarStyle = {
+        /*display: this.state.scrolling ? 'none' : 'block',*/
+        display: 'block',
+        transition: '2s'
+    }
+
+    render() {
+        return (
+            <div className={"main_container"} id={"root"}>
+                <div className={'NavBar'} style={{opacity: this.state.scrolling ? '0' : '1'}}>
+                    <NavBar />
+                </div>
+                <Home/>
+                <AboutMe/>
+                <Education/>
+                <Projects/>
+                <ContactMe/>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+/*
 function App() {
-  return (
-    <div className={"main_container"} id={"root"}>
-        <BrowserRouter>
-            <NavBar />
-            <Home />
-            <AboutMe />
-            <Education />
-            <Projects />
-            <ContactMe />
-            {/*<Routes>
+    return (
+        <div className={"main_container"} id={"root"}>
+            <BrowserRouter>
+                <NavBar />
+                <Home />
+                <AboutMe />
+                <Education />
+                <Projects />
+                <ContactMe />
+                {/!*<Routes>
                 <Route path={"/"} element={<Home />} />
                 <Route path={'/education'} element={<Education />} />
                 <Route path={'/aboutme'} element={<AboutMe />} />
                 <Route path={'/projects'} element={<Projects />} />
                 <Route path={'/contactme'} element={<ContactMe />} />
-            </Routes>*/}
-            <Footer />
-        </BrowserRouter>
+            </Routes>*!/}
+                <Footer />
+            </BrowserRouter>
 
-    </div>
-  );
-}
-
-export default App;
+        </div>
+    );
+}*/
